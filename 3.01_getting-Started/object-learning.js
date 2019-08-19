6eae5ffa0d03e89ac2bfba1a7573e4490a11d500
princeblack <mahamadicongo@hotmail.com>
2019-08-19 15:11:14 +0200

    Object.entries creates an array within an array

--- 3.01_getting-Started/object-learning.js
+++ 3.01_getting-Started/object-learning.js
@@ -115,3 +115,122 @@ console.log(fruitBuchet["kiwi red"]); // if let fruit = "kiwi red"
 console.log(fruitBuchet); // {kiwi : 5}
 
 
+// Reserved words (inbuilt words of the javascript ) are allowed as property names
+
+let obj1 = {
+    for : 1,
+    let : 2,
+    return : 3
+}
+console.log(obj1.for + obj1.let + obj1.return ); // 6
+
+let obj2 = {
+    for :" return",
+    let : 2,
+    return : 3
+}
+console.log(obj2.for + obj2.let + obj2.return ); // return23
+
+let obj3 = {
+    for : 1,
+    let : "return",
+    return : 3
+}
+console.log(obj3.for + obj3.let + obj3.return ); // 1return3
+
+
+
+// comparion object
+console.log('cOMPARING OBKJECTS');
+
+let obj4 = {
+    name : {
+        firstName : 'John',
+        lastName : 'Schulz'
+    },
+    age : 32
+};
+
+
+let obj5 = {
+    name : {
+        firstName : 'John',
+        lastName : 'Schulz'
+    },
+    age : 32
+};
+
+console.log(obj4 == obj5); //false
+console.log(obj4.name === obj5.name); // false
+console.log(obj4.name.firstName == obj5.name.firstName);
+
+// looping through Objects
+
+// the better way to loop through objects is first to convert the object into an arry . Than you loop throuh the array .
+//  you can convert an object into an  array with three methods.
+// 1. Object.keys
+// 2. Object.values
+// 3. Object.entries 
+
+// 1. Object.keys : creates an array that contains the properties of an object
+const fruits ={
+    apple : 28,
+    orange : 17,
+    pear : 54
+}
+console.log(fruits);
+
+const keys = Object.keys(fruits)
+console.log(keys);
+
+//  looping through arrays created from Object.keys
+console.log("--- Case 1 ---");
+// case 1 : old way
+
+for (let i = 0; i < keys.length; i++) {
+    console.log(keys[i]);
+
+}
+
+console.log("--- Case 2 ---");
+// case 2 : new way
+for (const key of  keys) {
+    console.log(key);
+    
+}
+
+// case 3 : array.forEach (function  (currentValue, index, arr), thisValue)
+// thisValue : OPtional . A value to be passed to the function to be used as its "this" value.
+//  If this parameter is empty, the value "undefined" will be passed as its "this" value
+
+console.log("--- Case 3 ---");
+console.log(keys);
+keys.forEach(displayKeys);
+function displayKeys(item, jeSuisLa,keys) {
+    console.log(`The key ${item} has index ${jeSuisLa}`);
+}
+
+
+// in short form
+keys.forEach((item,index)=>{
+    console.log(`The key ${item} has index ${index}`);
+
+});
+
+// 2. Object.values creates an array that contains the values of every property in an object
+
+const values = Object.values(fruits);
+console.log(values);
+
+// for(const item of values){
+//     values [item]= "test";
+//     console.log([item]);
+
+    
+    
+// }
+
+//  Object.entries creates an array within an array (an array of arrays). Each inner array has two item. The first item is the property and second item is the value.
+
+const entries = Object.entries(fruits);
+console.log(entries);
