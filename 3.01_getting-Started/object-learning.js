ab25229c9495f7d5423b246d9980b931246f7b79
princeblack <mahamadicongo@hotmail.com>
2019-08-19 11:00:14 +0200

    access the value medium of the size property

--- 3.01_getting-Started/object-learning.js
+++ 3.01_getting-Started/object-learning.js
@@ -25,3 +25,48 @@ console.log(myNewCar);
 console.log(`The property make : ${myNewCar.make}`);
 console.log(`The property model : ${myNewCar.model}`);
 console.log(`The property year : ${myNewCar.year}`);
+
+// Array info
+var info = ['John',' Schulz', 32, 'Berlin']
+console.log(info[0]); 
+
+//  objetct info
+
+let objInfo = {
+    firstName : 'John',
+    lastName : 'Schulz',
+    age : 32,
+    city : 'Berlin'
+}
+console.log(objInfo.lastName);
+
+var family ={
+    children : [{age :3, name: 'paul'},{age : 4, name: 'susi'}],
+    father :  'peter',
+    mother : 'sandra'
+};
+console.log(family.children[1].name);
+console.log(family);
+
+var appletree = {
+    apples :[
+        {
+            color : 'red',
+            taste : 'very',
+            num : 12,
+            size : [
+                'big',
+                'small',
+                'medium'
+            ]
+        },
+        {
+            color : 'yellow',
+            tast : 'very',
+            num : 11
+        }
+    ],
+    tree : 'wonderful'
+}
+//  access the value medium of the size property.
+console.log( appletree.apples[0].size[2]);
