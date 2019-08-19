bcd481324ced7b323ff648157b1ac6e4828ee55b
princeblack <mahamadicongo@hotmail.com>
2019-08-19 09:53:21 +0200

    assigning values to properties

---
+++ 3.01_getting-Started/object-learning.js
@@ -0,0 +1,27 @@
+// creating a object
+
+let myCar = {
+    // key : value,
+    make : 'Ford',
+    model : 'Mustang',
+    year : 1969
+};
+
+console.log(myCar);
+console.log(`The property make : ${myCar.make}`);
+console.log(`The property model : ${myCar.model}`);
+console.log(`The property year : ${myCar.year}`);
+
+
+//   assigning values to properties
+
+let myNewCar ={};
+
+myNewCar.make = 'Ford';
+myNewCar.model = 'Mustang';
+myNewCar.year = 1969;
+
+console.log(myNewCar);
+console.log(`The property make : ${myNewCar.make}`);
+console.log(`The property model : ${myNewCar.model}`);
+console.log(`The property year : ${myNewCar.year}`);
