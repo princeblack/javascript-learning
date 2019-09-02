782135cb08d034132a52ca3c0d31885f4a90665f
princeblack <mahamadicongo@hotmail.com>
2019-09-02 13:42:52 +0200

    Start Class

--- 3.01_getting-Started/28-08-exercise.js
+++ 3.01_getting-Started/28-08-exercise.js
@@ -154,14 +154,12 @@ console.log('::::::::::::::::::::::::::::::::::::::::');
 
 var arrays = [["1", "2", "3"],[true],[4, 5, 6]];
 
-let test = [];
 const flatten = arrays.reduce((acc,val)=>{
-    
+    acc = acc.concat(val);
     return acc;
 },[])
 console.log(flatten);
 console.log('::::::::::::::::::::::::::::::::::::::::');
-
 
 /////////////////////////////////////////
 //////////////////MAP////////////////////
@@ -169,37 +167,98 @@ console.log('::::::::::::::::::::::::::::::::::::::::');
 // Make an array of numbers that are doubles of the first array
 // call: doubleNumbers([2, 5, 100]); 
 // output: [4, 10, 200]
+
+let simpleNum =[2, 5, 100];
+const doubleNumbers = simpleNum.map(x => x *2);
+console.log(doubleNumbers);
+console.log('::::::::::::::::::::::::::::::::::::::::');
+
 // Take an array of numbers and make them strings
 // call: doubleNumbers([2, 5, 100]); 
 // output: ['2', '5', '100']
+let nums1 = [2, 5, 100];
+const double = nums1.map((x)=>{
+    let res = JSON.stringify(x)
+    return res
+})
+console.log(double);
+console.log('::::::::::::::::::::::::::::::::::::::::');
+
 // Capitalize each of an array of names
 // call: capitalizeNames(["john", "JACOB", "jinGleHeimer", "schmidt"]); 
 // output: ["John", "Jacob", "Jingleheimer", "Schmidt"]
+
+
+function capitalizeNames (letter) {
+       
+ 
+    const res = letter.map((x)=>{
+        if (x) {
+            x = x.toLowerCase()
+        }        
+        for (let i = 0; i < x.length; i++) {
+            if (x[0] == x[i][0]) {
+                x[0] = x[i][0].toUpperCase()
+                console.log(x);
+            }
+            // for (let j = 0; j < x.length; j++) {
+            //     console.log(x[i][0]);
+                
+                
+            // }
+        }
+        // console.log(x);
+        
+        
+    });
+ 
+}
+capitalizeNames(["john", "JACOB", "jinGleHeimer", "schmidt"]);
+
+
+console.log('::::::::::::::::::::::::::::::::::::::::');
+
+
 // Make an array of strings of the names
-// let arr = [
-//     {
-//         name: "Angelina Jolie",
-//         age: 80
-//     },
-//     {
-//         name: "Eric Jones",
-//         age: 2
-//     },
-//     {
-//         name: "Paris Hilton",
-//         age: 5
-//     },
-//     {
-//         name: "Kayne West",
-//         age: 16  
-//     },
-//     {
-//         name: "Bob Ziroll",
-//         age: 100
-//     }
-// ]; 
+
 // call: takeNames(arr);
 // output: ["Angelina Jolie", "Eric Jones", "Paris Hilton", "Kayne West", "Bob Ziroll"]
+
+let arrs = [
+    {
+        name: "Angelina Jolie",
+        age: 80
+    },
+    {
+        name: "Eric Jones",
+        age: 2
+    },
+    {
+        name: "Paris Hilton",
+        age: 5
+    },
+    {
+        name: "Kayne West",
+        age: 16  
+    },
+    {
+        name: "Bob Ziroll",
+        age: 100
+    }
+]; 
+function takeNames(array) {
+    let names =[];
+    const getName = array.map((value)=>{
+        if (value.name) {
+            JSON.parse(JSON.stringify(value.name))
+            names.push(value.name)      
+        }
+    });
+    return names
+}
+console.log(takeNames(arrs));
+console.log('::::::::::::::::::::::::::::::::::::::::');
+
 // Using the same array above as input, write a function that returns an array of strings of the names saying whether or not they can go to The Matrix. In order to go to the Matrix you need to be at least 18
 // call: whoCanGo(arr)
 // output: 
@@ -208,101 +267,231 @@ console.log('::::::::::::::::::::::::::::::::::::::::');
 // "Paris Hilton is under age!!", 
 // "Kayne West is under age!!", 
 // "Bob Ziroll can go to The Matrix"]
+function whoCanGo(people) {
+    let allPeople = people;
+    const movieAge = allPeople.map((myAge)=>{
+        let movie = [];
+        if (myAge.age >= 18) {
+            movie.push(myAge.name + ' ' + 'can go to The Matrix');  
+        }else{
+            movie.push(myAge.name + ' ' + 'is under age!!');
+        }
+        console.log(movie);
+        
+    });
+}
+whoCanGo(arrs);
+
+console.log('::::::::::::::::::::::::::::::::::::::::');
+
 /////////////////////////////////////////
 ////////////////FILTER///////////////////
 /////////////////////////////////////////
 // Given an array of numbers, return a new array that has only the numbers that are 5 or greater.
 // call: fiveAndGreaterOnly([3, 6, 8, 2]); 
 // output: [6, 8]
+
+function fiveAndGreaterOnly(num) {
+    num = num.filter((x)=>{
+        if (x >= 5) {
+            console.log(x);
+            
+        }
+    })
+}
+
+
+fiveAndGreaterOnly([3, 6, 8, 2]);
+
+console.log('::::::::::::::::::::::::::::::::::::::::');
+
 // Given an array of numbers, return a new array that only includes the even numbers.
 // call: fiveAndGreaterOnly([3, 6, 8, 2]); 
 // output: [6, 8, 2]
+
+function fiveAndGreaterOnly2(num) {
+    num = num.filter((x)=>{
+        if (x % 2 == 0) {
+            console.log(x);
+        }
+    })
+}
+fiveAndGreaterOnly2([3, 6, 8, 2])
+console.log('::::::::::::::::::::::::::::::::::::::::');
+
+
 // Given an array of strings, return a new array that only includes those that are 5 characters or fewer in length
 // call: fiveCharactersOrFewerOnly(["dog", "wolf", "by", "family", "eaten", "camping"]); 
 // output"["by", "dog", "wolf", "eaten"] 
-// Write a function that will return a new array of people that are members of the Illuminati
-//let arr =[
-//     {
-//         name: "Angelina Jolie",
-//         member: true
-//     },
-//     {
-//         name: "Eric Jones",
-//         member: false
-//     },
-//     {
-//         name: "Paris Hilton",
-//         member: true
-//     },
-//     {
-//         name: "Kayne West",
-//         member: false
-//     },
-//     {
-//         name: "Bob Ziroll",
-//         member: true
-//     }
-// ]
+
+
+function fiveCharactersOrFewerOnly(array) {
+    let newArray = [];
+    array = array.filter((x)=>{
+        if (x.length <= 5) {
+            newArray.push(x);
+        }
+    });
+    return newArray;
+
+}
+console.log(fiveCharactersOrFewerOnly(["dog", "wolf", "by", "family", "eaten", "camping"]));
+console.log('::::::::::::::::::::::::::::::::::::::::');
+
+
+// Write a function that will return a new array of ple that are members of the Illuminati 
 //  call : whoIsAnIlluminati(arr)
+
+let array2 =[
+    {
+        name: "Angelina Jolie",
+        member: true
+    },
+    {
+        name: "Eric Jones",
+        member: false
+    },
+    {
+        name: "Paris Hilton",
+        member: true
+    },
+    {
+        name: "Kayne West",
+        member: false
+    },
+    {
+        name: "Bob Ziroll",
+        member: true
+    }
+];
+
+function whoIsAnIlluminati(people) {
+    let newArray =[];
+    people = people.filter((data)=>{
+        if (data.member == true) {
+            newArray.push(data.name)            
+        }
+    })
+    console.log(newArray);
+
+}
+whoIsAnIlluminati(array2)
+console.log('::::::::::::::::::::::::::::::::::::::::');
+
 // Filter out all the people who are not old enough to see The Matrix
-// let arr = [
-//     {
-//         name: "Angelina Jolie",
-//         age: 80
-//     },
-//     {
-//         name: "Eric Jones",
-//         age: 2
-//     },
-//     {
-//         name: "Paris Hilton",
-//         age: 5
-//     },
-//     {
-//         name: "Kayne West",
-//         age: 16  
-//     },
-//     {
-//         name: "Bob Ziroll",
-//         age: 100
-//     }
-// ]; 
-// call: canSeeMAtrix(arr);
+let arr3 = [
+    {
+        name: "Angelina Jolie",
+        age: 80
+    },
+    {
+        name: "Eric Jones",
+        age: 2
+    },
+    {
+        name: "Paris Hilton",
+        age: 5
+    },
+    {
+        name: "Kayne West",
+        age: 16  
+    },
+    {
+        name: "Bob Ziroll",
+        age: 100
+    }
+]; 
+
+function canSeeMAtrix(people) {
+    let newArray =[];
+    people = people.filter((data)=>{
+        if (data.age >= 18) {
+            newArray.push(data.name)
+        }
+    });
+    console.log(newArray);
+    
+}
+ canSeeMAtrix(arr3);
+ console.log('::::::::::::::::::::::::::::::::::::::::');
+
 /////////////////////////////////////////
 /////////////////SORT////////////////////
 /////////////////////////////////////////
 // Sort an array from smallest number to largest
 // call: fromSmallToLarge([1, 3, 5, 2, 90, 20])
 // ouput: [1, 2, 3, 5, 20, 90]
+function compare(a,b) {
+    return a - b;
+}
+function fromSmallToLarge(num) {
+    return num.sort(compare);
+    
+}
+console.log(fromSmallToLarge([1,3,5,2,90,20]));
+console.log('::::::::::::::::::::::::::::::::::::::::');
+
 // Sort an array from largest number to smallest
 // call: fromLargeToSmall([1, 3, 5, 2, 90, 20])
 // ouput: [90, 20, 5, 3, 2, 1]
+function large(a,b) {
+     return b - a;
+}
+function fromLargeToSmall(num) {
+    return num.sort(large)
+}
+
+console.log(fromLargeToSmall([1, 3, 5, 2, 90, 20]));
+console.log('::::::::::::::::::::::::::::::::::::::::');
 // Sort an array from shortest string to longest
 // call: lengthSort(["dog", "wolf", "by", "family", "eaten"]); 
 // output: ["by", "dog", "wolf", "eaten", "family"] 
+function length(a,b) {
+    return a.length - b.length
+}
+function lengthSort(word) {
+    return word.sort(length)
+}
+console.log(lengthSort(["dog", "wolf", "by", "family", "eaten"]));
+console.log('::::::::::::::::::::::::::::::::::::::::');
+
 // Sort an array alphabetically
 // call: alphabetical(["dog", "wolf", "by", "family", "eaten"]); 
 // output: ["by", "dog", "eaten", "family", "wolf"] 
+function alphabetical(alpha) {
+    return alpha.sort()
+}
+
+console.log(alphabetical(["dog", "wolf", "by", "family", "eaten"]));
+console.log('::::::::::::::::::::::::::::::::::::::::'); 
 // Sort the objects in the array by age
-// let arr = [
-//     {
-//         name: "Quiet Samurai",
-//         age: 22
-//     },
-//     {
-//         name: "Arrogant Ambassador",
-//         age: 100
-//     },
-//     {
-//         name: "Misunderstood Observer",
-//         age: 2
-//     },
-//     {
-//         name: "Unlucky Swami",
-//         age: 77
-//     }
-// ]
-//call: sortByAge(arr);
+let arr4 = [
+    {
+        name: "Quiet Samurai",
+        age: 22
+    },
+    {
+        name: "Arrogant Ambassador",
+        age: 100
+    },
+    {
+        name: "Misunderstood Observer",
+        age: 2
+    },
+    {
+        name: "Unlucky Swami",
+        age: 77
+    }
+];
+function compareAge(a,b) {
+    return a.age - b.age;
+}
+
+function sortByAge(byAge) {
+    return byAge.sort(compareAge)
+}
+console.log( sortByAge(arr4));
+
 // Collapse
 
 
---
+++ 3.01_getting-Started/class.learn.js
@@ -0,0 +1,117 @@
+ 
+// *******************************************CLASSES*******************************************
+// You can create an object using either "Object.create" or "new" keyword
+function Animal(name, energy) {
+    // let animal = Object.create(Animal.prototype);   //when calling with new keyword we don't need this line anymore
+    //animal.name = name;                           
+    this.name = name;               // to call it with the "new" keyword we have to add "this" keyword here and delete "animal" before name
+    //animal.energy = energy;
+    this.energy = energy;            // to call it with the "new" keyword we have to add "this" keyword here and delete "animal" before energy
+    // return animal;
+}
+Animal.prototype.eat = function (amount) {
+    console.log(`${this.name} is eating`);
+    this.energy += amount;
+}
+Animal.prototype.sleep = function (length) {
+    console.log(`${this.name} is sleeping`);
+    this.energy += length;
+}
+Animal.prototype.play = function (length) {
+    console.log(`${this.name} is playing`);
+    this.energy -= length;
+}
+// With Object.create we can call the above function like this:
+// const leo = Animal("Leo", 7);
+// const snoop = Animal("Snoop", 10);
+// console.log(leo);  // undefined
+// whenever we use the new keyword it means object instantiation.
+// now we remove Object.create and use new keyword
+const leo = new Animal("Leo", 7);
+const snoop = new Animal("Snoop", 10);
+//console.log(leo);  // returns object as Animal { name: 'Leo', energy: 7 }
+/************************************************************************************************ */
+// A class is a type of special function, but instead of using the keyword function to initiate it, we use the keyword "class", and the properties is assigned inside a constructor() method.
+// The class syntax has two components: class expressions and class declarations. Class declaration: to declare a class, you use the class keyword with the name of the class : ex: class Animal{...}
+// The constructor method is a special method for creating and initializing an object created within a class.
+// Use the keyword "class" to create a class, and always add a constructor method.
+// The constructor method is called each time the class object is initialized.
+// to reach inside a class use objectName.methodName  (leo.eat)
+class Animal2 {
+    constructor(name, energy) {
+        console.log("constructor...");
+        this.name = name;
+        this.energy = energy;
+    }
+    eat(amount) {
+        console.log(`${this.name} is eating`);
+        this.energy += amount;
+    }
+    sleep(length) {
+        console.log(`${this.name} is sleeping`);
+        this.energy += length;
+    }
+    play(length) {
+        console.log(`${this.name} is playing`);
+        this.energy -= length;
+    }
+    static nextToEat(animals) {
+        const sortedLeastEnergy = animals.sort((a, b) => {
+            return a.energy - b.energy;
+        });
+        console.log(sortedLeastEnergy);
+        return sortedLeastEnergy[0].name;
+    }
+}
+//In this case, eat,sleep and play are not belong to animal object, object is to reach the all the methods of the class
+const firstObj = new Animal2();
+
+// console.log(firstObj);
+
+//Output of the above is
+// constructor...   //because whenever you create a new object, it calls it with constructor.
+// Animal2 { name: undefined, energy: undefined }
+const secondObj = new Animal2("second", 1);
+// console.log(`constructor calling secondObj firs time`);
+// console.log(secondObj);
+//Output of the above is
+// constructor...
+// Animal2 { name: 'second', energy: 1 }
+firstObj.name = "first";
+firstObj.energy = 2;
+// console.log(`constructor calling secondObj second time`);
+// console.log(secondObj);  // Animal2 { name: 'second', energy: 1 }
+// console.log(`calling first obj`);
+// console.log(firstObj);  // Animal2 { name: 'first', energy: 2 }
+firstObj.name = "replace";
+firstObj.energy = 3;
+// console.log(`calling first obj after replace and 3`);
+// console.log(firstObj);  // Animal2 { name: 'replace', energy: 3 }
+//firstObj.eat(5);  // replace is eating
+//console.log(firstObj);   // Animal2 { name: 'replace', energy: 8 } // because when you call it firstObj.eat(5), it adds the energy(3) with given value(5) 
+/************************************************************************************************ */
+const leoObj = new Animal2("Leo", 7); // output is: constructor..  // because whenever you create a new object, it calls it with constructor. (leo is the instance of the Animal2 class or leo is an object of this Animal2 class)
+const snoopObj = new Animal2("Snoop", 10); // output is: constructor..
+//leoObj.eat(5);       // Leo is eating
+// console.log(leoObj.name);   // Leo
+// console.log(snoopObj.name);  // Snoop
+// console.log(leoObj);    //  Animal2 { name: 'Leo', energy: 12 }
+/*******************************************STATIC METHOD***************************************************** */
+// Static methods are defined on the class itself, and not on the prototype/object. That means you can not call a static method by using the name of the object.
+// If you want to access inside a static method, you can do it by using the class name (which is written after "static" keyword)
+// function nextToEat(animals) {                                  // this is the function we added above as static
+//     const sortedLeastEnergy = animals.sort((a, b) => {
+//         return a.energy - b.energy;
+//     });
+//     return sortedLeastEnergy[0].name;
+// }
+const leon = new Animal2("Leon,7");
+const snoopy = new Animal2("Snoopy", 10);
+//console.log(nextToEat([leon, snoopy]));  // Leon,7   // I am passing an array because sort method belongs to an array
+
+// console.log(Animal2.nextToEat([leon, snoopy]));  // Leon,7
+
+//console.log(Animal2.eat(5));   // eat is not static method and if you want to access a non static method you need to use the name of the object
+/************************************************************************************************ */
+// Task: Create a class called Person which accepts the name of a person as a string, and his / her age as a number.
+// The Person class should have a method called describe which returns a string with the following syntax: "name, age years old".So for example, if John is 19 years old then the function describe of his object will return "John, 19 years old".
