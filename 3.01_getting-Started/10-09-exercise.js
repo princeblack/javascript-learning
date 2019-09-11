6350f6d5fe97ccd4b59ba2aa69b30b06908be747
princeblack <mahamadicongo@hotmail.com>
2019-09-11 08:49:12 +0200

    eturns true if all integers in an array are factors of a number

--- 3.01_getting-Started/10-09-exercise.js
+++ 3.01_getting-Started/10-09-exercise.js
@@ -246,13 +246,15 @@ addEnding(["bend", "sharpen", "mean"], "ing")
 // Write a function that returns true if all integers in an array are factors of a number, and false otherwise.
 
 function checkFactors(factors, num) {
-	
+	return factors.every(x => num % x == 0 );
 }
 
-checkFactors([2, 3, 4], 12) //➞ true
+console.log(checkFactors([2, 3, 4], 12));
+ //➞ true
 // Since 2, 3, and 4 are all factors of 12.
 
-checkFactors([1, 2, 3, 8], 12) //➞ false
+console.log(checkFactors([1, 2, 3, 8], 12));
+ //➞ false
 // 8 is not a factor of 12.
 
 checkFactors([1, 2, 50], 100) //➞ true
