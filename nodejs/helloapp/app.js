8dcc976ce1461791d8205ae6fe60ebf4a7d19d53
princeblack <mahamadicongo@hotmail.com>
2019-09-19 14:24:35 +0200

    node npm

--- nodejs/helloapp/app.js
+++
@@ -1,9 +0,0 @@
-// const printSomething = require('./print');
-const {myfunction,ClassName}= require('./print');
-console.log("Starting the app....");
-// console.log(printSomething);
-console.log(`we are in classe ${ClassName}`);
-
-myfunction()
-const name = process.argv[2];
-console.log(`hello ${name}`);
--- nodejs/helloapp/print.js
+++
@@ -1,4 +0,0 @@
-module.exports.ClassName = 'Babylon';
-module.exports.myfunction = ()=>{
-    console.log('I am inside the print.js file');
-}
