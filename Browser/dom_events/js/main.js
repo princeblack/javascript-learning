faf4d19c85a5b52f54778134763990c20751f7d4
princeblack <mahamadicongo@hotmail.com>
2019-12-16 23:53:54 +0100

    fetch promise

--- Browser/dom_events/js/main.js
+++ Browser/dom_events/js/main.js
@@ -1,27 +1,39 @@
 let parent=  document.querySelector(".parent");
 let child = document.querySelector(".child");
-
+let a = 1;
+let b = 1;
 
 parentClick= e =>{
     console.log("somebody click on the parent");
-    parent.style.backround = "red";
-    parent.style.zIndex = "1";
-    if (parent.style.zIndex == 1) {
-        child.style.zIndex = "-1";
-    }
+    console.log(a);
+    console.log(b);
     
+    
+    parent.style.zIndex = a;
+    if (parent.style.zIndex == a) {
+        b = b -1;
+        child.style.zIndex = b;
+    }
+    a = 1;
+    b = 1;
 }
+
 
 childClick= e =>{
-    // e.stopPropagation()
-    if (parent.style.zIndex == 1) {
-        parent.style.zIndex = "-1";
-        child.style.zIndex = "1";
-
-    }
-    
+    e.stopPropagation()
     console.log("somebody click on the child");
+    console.log(a);
+    console.log(b);
+    child.style.zIndex = b;
+    if (child.style.zIndex == b) {
+        a = a -1
+        parent.style.zIndex = a;
+    }
+    a = 1;
+    b = 1;
 }
+
+
 parent.addEventListener("click",parentClick)
 child.addEventListener("click",childClick)
 
